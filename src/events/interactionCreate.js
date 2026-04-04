82b990429c29ff58889fe3dbb418e91b44552e5e
kdfuzzy <kyedow0412@gmail.com>
2026-04-04 03:09:20 +0100

    Add interactionCreate event handler

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "interactionCreate",
+
+    async execute(interaction, client) {
+        if (!interaction.isChatInputCommand()) return;
+
+        const command = client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction, client);
+        } catch (err) {
+            console.error(err);
+            interaction.reply({ content: "Error executing command", ephemeral: true });
+        }
+    }
+};
