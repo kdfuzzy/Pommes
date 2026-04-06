ff52c83e39bec6d9d8deb31eab63165de29a96ba
kdfuzzy <kyedow0412@gmail.com>
2026-04-06 16:37:13 +0100

    Update addmoney.js

--- commands/addmoney.js
+++ commands/addmoney.js
@@ -1,36 +1,24 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { addBalance } = require('../utils/economy');
-
-const ALLOWED_ROLE = '1480954571844092155';
+const { addBalance, formatSol } = require('../utils/economy');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('addmoney')
-        .setDescription('Add money to a user')
+        .setDescription('Add SOL to a user')
         .addUserOption(opt =>
             opt.setName('user').setDescription('User').setRequired(true))
-        .addIntegerOption(opt =>
-            opt.setName('amount').setDescription('Amount').setRequired(true)),
+        .addNumberOption(opt =>
+            opt.setName('amount').setDescription('Amount in SOL').setRequired(true)),
 
     async execute(interaction) {
-        const member = interaction.member;
-
-        if (!member.roles.cache.has(ALLOWED_ROLE)) {
-            return interaction.reply({
-                content: '❌ You don’t have permission.',
-                ephemeral: true
-            });
-        }
 
         const user = interaction.options.getUser('user');
-        const amount = interaction.options.getInteger('amount');
+        const amount = interaction.options.getNumber('amount');
 
-        if (amount <= 0) {
-            return interaction.reply({ content: '❌ Invalid amount.', ephemeral: true });
-        }
+        if (amount <= 0) return interaction.reply('❌ Invalid amount.');
 
         addBalance(user.id, amount);
 
-        await interaction.reply(`💰 Added **${amount}** to ${user.username}`);
+        interaction.reply(`➕ Added ${formatSol(amount)} to ${user.username}`);
     }
 };
