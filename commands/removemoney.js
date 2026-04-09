6adf61c3fc07fc3ca592435e98400e5bf56ee8b6
kdfuzzy <kyedow0412@gmail.com>
2026-04-09 19:55:02 +0100

    Update removemoney.js

--- commands/removemoney.js
+++ commands/removemoney.js
@@ -1,22 +1,32 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { removeBalance } = require('../utils/economy');
+const { removeBalance, formatSol } = require('../utils/economy');
+
+const OWNER_ID = '794606718972723230';
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('removemoney')
         .setDescription('Remove money from a user')
         .addUserOption(opt =>
-            opt.setName('user').setDescription('User').setRequired(true))
-        .addIntegerOption(opt =>
-            opt.setName('amount').setDescription('Amount').setRequired(true)),
+            opt.setName('user')
+                .setDescription('User')
+                .setRequired(true))
+        .addNumberOption(opt =>
+            opt.setName('amount')
+                .setDescription('Amount')
+                .setRequired(true)),
 
     async execute(interaction) {
 
+        if (interaction.user.id !== OWNER_ID) {
+            return interaction.reply({ content: '❌ You cannot use this.', ephemeral: true });
+        }
+
         const user = interaction.options.getUser('user');
-        const amount = interaction.options.getInteger('amount');
+        const amount = interaction.options.getNumber('amount');
 
         removeBalance(user.id, amount);
 
-        interaction.reply(`💸 Removed ${amount} from ${user.username}`);
+        interaction.reply(`💸 Removed ${formatSol(amount)} from ${user.username}`);
     }
 };
